e75f82aaa52ef56fd4eaa83d162f187f97775809
aditya <adityasharma346t@gmail.com>
2025-12-17 12:29:12 +0530

    updated env formatting

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,11 @@
 // Firebase config (from project)
 const firebaseConfig = {
-  apiKey: "AIzaSyCEp4u8R_9sJf15BtN35nYJdYUptmr42js",
-  authDomain: "ai-smart-public-complaint.firebaseapp.com",
-  projectId: "ai-smart-public-complaint",
-  storageBucket: "ai-smart-public-complaint.firebasestorage.app",
-  messagingSenderId: "443095807586",
-  appId: "1:443095807586:web:8e69f1c1ca682a22ecc33b"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 // Initialize Firebase (compat SDK expected)
